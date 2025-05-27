392a67aa4026fc2a3d1bd37b9d97753fc5793a87
Yusuke Wada <yusuke@kamawada.com>
2025-05-27 22:18:09 +0900

    fix: correct importing `traverse`

--- src/plugin/auto-entry.ts
+++ src/plugin/auto-entry.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { parse } from '@babel/parser'
-import traverse from '@babel/traverse'
+import _traverse from '@babel/traverse'
 import type { Plugin } from 'vite'
 import fs from 'node:fs'
 import path from 'node:path'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const traverse = (_traverse.default as typeof _traverse) ?? _traverse
 
 interface ComponentConfig {
   name: string
